d9241e6f9e4bc3bec8edcd8f2870c25877dd172a
Kontores <ethereal_alpha@hotmail.com>
2022-11-26 21:04:45 +0200

    Add project files.

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public record Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; init; }
+
+        [Required]
+        public string Publisher { get; init; }
+
+        [Required]
+        public string Cost { get; init; }
+    }
+}
