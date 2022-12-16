8fee01bd7e159d4c871be434459cfe8870c4fa02
Kontores <ethereal_alpha@hotmail.com>
2022-12-16 12:19:20 +0200

    command service models created

---
+++ CommandsService/Models/Command.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
+{
+    public record Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; init; }
+
+        [Required]
+        public string HowTo { get; init; }
+
+        [Required]
+        public string CommandLine { get; init; }
+
+        [Required]
+        public int PlatformId { get; init; }
+        public Platform Platform { get; init; }
+    }
+}
---
+++ CommandsService/Models/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
+{
+    public record Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; init; }
+
+        [Required]
+        public int ExternalId { get; init; }
+
+        [Required]
+        public string Name { get; init; }
+        public ICollection<Command> Commands { get; init; } = new List<Command>();
+    }
+}
